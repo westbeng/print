b537ca61dc9c6d63a155090eb630cf58f7911114
Sk Suraj <suraj9five@gmail.com>
2020-05-23 12:08:13 +0530

    4th commit

--- print/src/main/java/com/westbeng/print/Print.java
+++ print/src/main/java/com/westbeng/print/Print.java
@@ -11,22 +11,46 @@ import android.widget.Toast;
 
 public class Print {
 
-    public static void d(Context context, String functionName, String message) {
+    // Debug messages
+    public static void d(Context context, String message) {
+        Log.d(context.getClass().getSimpleName(), message);
+    }
+
+    public static void d(Context context, String message, boolean showToast) {
+        Log.d(context.getClass().getSimpleName(), message);
+        if (showToast) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static void d(Context context, String message, String functionName) {
         Log.d(context.getClass().getSimpleName(), functionName + " -> " + message);
     }
 
-    public static void d(Context context, String functionName, String message, boolean showToast) {
+    public static void d(Context context, String message, String functionName, boolean showToast) {
         Log.d(context.getClass().getSimpleName(), functionName + " -> " + message);
         if (showToast) {
             Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
         }
     }
 
-    public static void e(Context context, String functionName, String message) {
+    // Error messages
+    public static void e(Context context, String message) {
+        Log.e(context.getClass().getSimpleName(), message);
+    }
+
+    public static void e(Context context, String message, boolean showToast) {
+        Log.e(context.getClass().getSimpleName(), message);
+        if (showToast) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static void e(Context context, String message, String functionName) {
         Log.e(context.getClass().getSimpleName(), functionName + " -> " + message);
     }
 
-    public static void e(Context context, String functionName, String message, boolean showToast) {
+    public static void e(Context context, String message, String functionName, boolean showToast) {
         Log.e(context.getClass().getSimpleName(), functionName + " -> " + message);
         if (showToast) {
             Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
